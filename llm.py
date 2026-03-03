4c8b03d68e0fbc940472a1909e30db2d6d30987d
cristian.iorga <cristian.iorga@upc.edu>
2026-03-03 08:50:50 +0100

    Added dependencies for using the gemini model. Implemented a test for it.

--- llm.py
+++ llm.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from hri - llm.py!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    
+    api_key = os.getenv("GEMINI_API_KEY")
+    if not api_key:
+        print("ERROR NO API KEY DETECTED")
+        return
+
+    client = genai.Client(api_key=api_key)
+
+    prompt = "You are a robot assistant. You must provide help to the user. On the table the camera sees 2 bananas, but the user insists on wanting the apple. What do you do in this situation?"
+
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.5-flash-lite',
+            contents=prompt,
+        )
+        
+        print("\n Gemini Response:")
+        print(response.text)
+        
+    except Exception as e:
+        print(f"ERROR: {e}")
 
 if __name__ == "__main__":
     main()
